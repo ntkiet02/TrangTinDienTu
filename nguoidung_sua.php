0ffffeefedda0acb1d16faed2094d2782138027c
HanBui10 <buih72436@gmai.com>
2023-11-17 12:00:04 +0700

    Update nguoidung_sua.php

--- nguoidung_sua.php
+++ nguoidung_sua.php
@@ -6,7 +6,7 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<title>Sửa người dùng - iNews</title>
+		<title>Sửa người dùng - Trang tin công nghệ</title>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" />
 	</head>
