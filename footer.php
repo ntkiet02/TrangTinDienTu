b6edd41cfff04cfd871c61f82e7ac0c3cf028bc2
ntrkiet02 <ntrkiet@gmail.com>
2023-11-17 09:10:44 +0700

    Chỉnh sửa thanh navabar và footer

--- footer.php
+++ footer.php
@@ -1,2 +1,14 @@
-<hr />
-<footer>&copy; 2023 bởi Nguyễn Hoàng Tùng.</footer>
+<footer class="site-footer bg-light">
+		<div class="container">
+			<ul class="nav justify-content-center">
+				<li class="nav-item"><a class="nav-link" href="#">Privacy policy</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Terms</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Feedback</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Advertise</a></li>
+				<li class="nav-item"><a class="nav-link" href="page-contact.html">Contact</a></li>
+			</ul>
+			<div class="copy">
+				&copy; 2023 by Nghĩa, Nga, Nghi, Hân, Kiệt
+			</div>
+		</div>
+</footer>
