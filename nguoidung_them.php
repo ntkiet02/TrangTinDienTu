94f70a899aaa0b6958eb3c73c51c70e540413c7c
HanBui10 <buih72436@gmai.com>
2023-11-21 16:12:47 +0700

    update nguoidung_them

--- nguoidung_them.php
+++ nguoidung_them.php
@@ -6,14 +6,14 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<title>Thêm người dùng - Trang tin công nghệ</title>
+		<title>Thêm người dùng - iOlds</title />
 		<link rel="shortcut icon" href="images/premium.png" type="image/x-icon" />
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 	</head>
 	<body>
 		<div class="container">
-			<?php include_once 'navbar.php'; ?>
+			<?php include_once "navbar.php"; ?>
 			
 			<div class="card mt-3">
 				<h5 class="card-header">Thêm người dùng</h5>
@@ -67,7 +67,7 @@
 				</div>
 			</div>
 			
-			<?php include_once 'footer.php'; ?>
+			<?php include_once "footer.php"; ?>
 		</div>
 		
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
