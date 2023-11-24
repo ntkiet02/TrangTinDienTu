313796e5a764ec90b0b8c7fedafadb32e489558b
NgocNgaNguyen <ntnnga99@gmail.com>
2023-11-24 14:21:22 +0700

    xóa bài viết

---
+++ baiviet_xoa.php
@@ -0,0 +1,21 @@
+<?php
+	require_once 'ketnoi.php';
+	$id = $_GET['id'];
+	try
+	{
+		$sql='DELETE FROM tbl_baiviet WHERE ID = :ID';
+		$cmd=$conn->prepare($sql);
+		$cmd->bindValue(':ID', $id);
+		$result=$cmd->execute();
+		
+		$message = 'Đã xóa bài viết thành công';
+		include_once 'success.php';
+		
+	}
+	catch(PDOException $e)
+	{
+		echo $e->getMessage();
+	}
+
+
+?>
