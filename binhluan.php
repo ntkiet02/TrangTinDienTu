26e45c0b1c5cee58bc09d717eb10f95535247644
ntrkiet02 <ntrkiet@gmail.com>
2023-11-24 14:26:25 +0700

    phần bình luận

---
+++ binhluan.php
@@ -0,0 +1,36 @@
+
+<!-- / -->
+        <h3>2 bình luận</h3>
+        <div class="d-flex mb-3 p-3 bg-light">
+            <div class="text-center">
+                <img class="me-3 rounded-circle" src="images/avatars/1.png" width="100" height="100" />
+                <h6 class="mt-1 mb-0 me-3">Kim</h6>
+            </div>
+            <div class="flex-grow-1">
+                <p class="mt-3 mb-2">Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis.</p>
+                <time class="timeago text-muted" datetime="2021-09-20 20:00">20 november 2021</time>
+            </div>
+        </div>
+        <div class="d-flex mb-3 p-3 bg-light">
+            <div class="text-center">
+                <img class="me-3 rounded-circle" src="images/avatars/2.png" width="100" height="100" />
+                <h6 class="mt-1 mb-0 me-3">Paula</h6>
+            </div>
+            <div class="flex-grow-1">
+                <p class="mt-3 mb-2">Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus.</p>
+                <time class="timeago text-muted" datetime="2021-09-05 20:00">5 november 2021</time>
+            </div>
+        </div>
+        <div class="mt-5">
+            <h5>Write a response</h5>
+            <div class="row">
+                <div class="col-md-6">
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Your name">
+                </div>
+                <div class="col-md-6">
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Your email">
+                </div>
+            </div>
+            <textarea class="form-control mt-3" rows="3" placeholder="Write a response.."></textarea>
+            <a href="#" class="btn btn-success mt-3">Publish</a>
+        </div>
