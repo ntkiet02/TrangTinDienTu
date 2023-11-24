653711413e75119661fb83ceae21f5453e776ec3
ntrkiet02 <ntrkiet@gmail.com>
2023-11-24 10:17:28 +0700

    Chinhr sửa thanh navarbar

--- index_header.php
+++ index_header.php
@@ -30,12 +30,69 @@
 				<a class="navbar-brand mx-auto order-1 order-md-3" href="index.php">iOlds</a>
 				<div class="collapse navbar-collapse order-4 order-md-4" id="navbar-right">
 					<ul class="navbar-nav ms-auto">
+					<?php
+					if(isset($_SESSION['ID'])) // Nếu đã đăng nhập
+					{
+				?>
+						<?php
+							if($_SESSION['QuyenHan'] == 1) // Nếu là Quản trị viên
+							{
+						?>
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+										<i class="bi bi-database-gear"></i> Quản lý
+									</a>
+									<ul class="dropdown-menu">
+										<li><a class="dropdown-item" href="chude.php"><i class="bi bi-diagram-2"></i> Chủ đề</a></li>
+										<li><a class="dropdown-item" href="nguoidung.php"><i class="bi bi-people"></i> Người dùng</a></li>
+										<li><hr class="dropdown-divider"></li>
+										<li><a class="dropdown-item" href="baiviet.php"><i class="bi bi-stickies"></i> Bài viết</a></li>
+									</ul>
+								</li>
+						<?php
+							}
+							else
+							{
+						?>
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+									<i class="bi bi-person-circle"></i> <?php echo $_SESSION['HoVaTen']; ?>
+								</a>
+								<ul class="dropdown-menu">
+									<li><a class="dropdown-item" href="baiviet_them.php"><i class="bi bi-pencil-square"></i> Đăng bài viết</a></li>
+									<li><a class="dropdown-item" href="baiviet_cuatoi.php"><i class="bi bi-person-badge"></i> Bài viết của tôi</a></li>
+									<li><hr class="dropdown-divider"></li>
+									<li><a class="dropdown-item" href="nguoidung_doimatkhau.php"><i class="bi bi-key"></i> Đổi mật khẩu</a></li>
+								</ul>
+							</li>
+						<?php
+							}
+						?>
+						
 						<li class="nav-item">
+							<a class="nav-link" href="dangxuat.php"><i class="bi bi-box-arrow-right"></i> Đăng xuất</a>
+						</li>
+				<?php
+					}
+					else // Nếu chưa đăng nhập
+					{
+				?>
+						<li class="nav-item">
+							<a class="nav-link" href="dangky.php"><i class="bi bi-person-plus"></i> Đăng ký</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="dangnhap.php"><i class="bi bi-box-arrow-in-right"></i> Đăng nhập</a>
+						</li>
+				<?php
+					}
+				?>
+
+						<!-- <li class="nav-item">
 							<a class="nav-link" href="dangnhap.php">Đăng nhập</a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" href="dangky.php">Đăng ký</a>
-						</li>
+						</li> -->
 					</ul>
 					<form class="form-inline" role="search">
 						<input class="search js-search form-control form-control-rounded me-sm-2" type="text"
