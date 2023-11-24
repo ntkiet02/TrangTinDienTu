ff37c5b76f7ba2008860d8ebc05dab13ed114ee7
ntrkiet02 <ntrkiet@gmail.com>
2023-11-24 14:33:27 +0700

    sua lại bình luận

--- binhluan.php
+++ binhluan.php
@@ -4,21 +4,21 @@
         <div class="d-flex mb-3 p-3 bg-light">
             <div class="text-center">
                 <img class="me-3 rounded-circle" src="images/avatars/1.png" width="100" height="100" />
-                <h6 class="mt-1 mb-0 me-3">Kim</h6>
+                <h6 class="mt-1 mb-0 me-3">Hà Dĩ Khang</h6>
             </div>
             <div class="flex-grow-1">
-                <p class="mt-3 mb-2">Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis.</p>
-                <time class="timeago text-muted" datetime="2021-09-20 20:00">20 november 2021</time>
+                <p class="mt-3 mb-2">Tôi năm nay hơn 70 tuổi rồi mà tui chưa thấy trưòng hợp nào code ăn gian như vầy. Phải tui là tui làm full stak rùi</p>
+                <time class="timeago text-muted" datetime="2021-09-20 20:00">20/10/2023</time>
             </div>
         </div>
         <div class="d-flex mb-3 p-3 bg-light">
             <div class="text-center">
                 <img class="me-3 rounded-circle" src="images/avatars/2.png" width="100" height="100" />
-                <h6 class="mt-1 mb-0 me-3">Paula</h6>
+                <h6 class="mt-1 mb-0 me-3">Xuân Dịu</h6>
             </div>
             <div class="flex-grow-1">
-                <p class="mt-3 mb-2">Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus.</p>
-                <time class="timeago text-muted" datetime="2021-09-05 20:00">5 november 2021</time>
+                <p class="mt-3 mb-2">Bài báo này đã làm tâm hồn tui trở nên thanh thản vì đọc hong hiểu</p>
+                <time class="timeago text-muted" datetime="2021-09-05 20:00">20/10/2023</time>
             </div>
         </div>
         <div class="mt-5">
